e0c4b9c7a1a96a22db18281c9ac3456462dd2830
Shreyansh Saha <erixerns@gmail.com>
2018-09-16 16:43:42 +0530

    Inital Commit

---
+++ webInterface.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import gps
+import time
+import serial
+from flask import Flask, render_template, request, jsonify
+# import threading
+
+
+app = Flask(__name__)
+arduino=serial.Serial("/dev/ttyACM0", 9600)
+response = dict()
+
+# ==========
+# GPS Socket
+# ==========
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+# =========
+# Variables
+# =========
+isAuto = False
+initialized = False
+queue = []
+location=[]
+
+currentLocation=dict({})
+nextLocation='NoR'
+textPastCommand='NoR'
+err='None'
+gpsErr='None'
+
+@app.route("/")
+def sendData():
+	return render_template("home.html")
+
+
+@app.route("/getData", methods=['GET'])
+def gpsData():
+	global initialized
+	report = session.next()
+	print(report)
+	response['report']=dict({'dat':"YES"})
+	if report['class']=='TPV':
+		gpsErr='None'
+		if hasattr(report, 'time') and hasattr(report, 'lon') and hasattr(report, 'lat'):
+				currentLocation['lon']=report.lon
+				currentLocation['lat']=report.lat
+				if not initialized:		
+					initialized=True
+					location.append([report.lon, report.lat])
+	else:
+		currentLocation['lat']='Nor'
+		currentLocation['lon']='Nor'
+		gpsErr='GPS is not Recieving values'
+
+	response['textPastCommand']=textPastCommand
+	response['gpsError']=gpsErr
+	response['currentLocationLat']=currentLocation['lat']
+	response['currentLocationLon']=currentLocation['lon']
+	response['nextLocation']=nextLocation
+	return jsonify(**response)
+
+@app.route("/getSensor", methods=['GET', 'POST'])
+def returnSensorData():
+	data=arduino.readline()
+	data = data.decode('utf-8').rstrip()
+	data=data.split(' ')
+	ph, temp, moisture = data
+	return jsonify(sensorDataPH=ph, sensorDataTemp=temp, sensorDataMoisture=moisture)
+
+app.debug=True
+app.run('192.168.43.30', debug=True)
