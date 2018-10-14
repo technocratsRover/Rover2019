6851bb040f25aba6fea65b70d843758914bbb3fb
erixerns <erixerns@gmail.com>
2018-10-14 18:47:55 +0530

    Added threading for pycam

--- webInterface/webInterface.py
+++ webInterface/webInterface.py
@@ -4,6 +4,12 @@ import nanpy
 import collections
 from nanpy import ArduinoApi, SerialManager
 from flask import Flask, render_template, request, jsonify
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2 as cv
+import numpy as np
+import threading
 # import threading
 
 # =========
@@ -31,6 +37,126 @@ gpsErr='None'
 arduinoErr='None'
 # Dictionary to store response
 response = dict()
+
+# =============
+# Obj Detection
+# =============
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+
+#Setting up frames
+openingKernel = cv.getStructuringElement(cv.MORPH_RECT, (10, 10)) # Make a kernel for closing (noise removal)
+openingKernelRed = cv.getStructuringElement(cv.MORPH_RECT, (2, 2))
+
+# define range of blue color in HSV
+# Range for green in HSV
+# lower = 40 40 40
+# upper = 80 255 255
+# TODO: Tweek these values to detect all ranges of color properly
+
+# Values calculated from: https://alloyui.com/examples/color-picker/hsv
+# Limits for green range
+lower_green = np.array([37, 80, 40])
+upper_green = np.array([95, 255, 255])
+
+# Limits for blue range
+lower_blue = np.array([100, 80, 50])
+upper_blue = np.array([125, 255, 255])
+
+# Limits for yellow range
+lower_yellow = np.array([20, 80, 40])
+upper_yellow = np.array([35, 255, 255])
+
+# Limits for red
+lower_red = np.array([170, 80, 40])
+upper_red = np.array([180, 255, 255])
+
+
+objOutput = None
+
+def objThread():
+	global objOutput
+	# capture frames from the camera
+	while True:
+		for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+		os.system('clear')
+		# grab the raw NumPy array representing the image, then initialize the timestamp
+		# and occupied/unoccupied text
+		frame = frame.array
+		hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV) # Take each frame
+
+		# Threshold the HSV image to get only desired colors
+		maskGreen = cv.inRange(hsv, lower_green, upper_green)
+		maskBlue = cv.inRange(hsv, lower_blue, upper_blue)
+		maskYellow = cv.inRange(hsv, lower_yellow, upper_yellow)
+		maskRed = cv.inRange(hsv, lower_red, upper_red)
+
+
+		# # Clear noise from mask by opening it, then apply gaussian blur
+		maskGreen = cv.morphologyEx(maskGreen, cv.MORPH_OPEN, openingKernel)
+		maskGreen = cv.GaussianBlur(maskGreen, (5, 5), 0)
+
+		maskBlue = cv.morphologyEx(maskBlue, cv.MORPH_OPEN, openingKernel)
+		maskBlue = cv.GaussianBlur(maskBlue, (5, 5), 0)
+
+		maskYellow = cv.morphologyEx(maskYellow, cv.MORPH_OPEN, openingKernel)
+		maskYellow = cv.GaussianBlur(maskYellow, (5, 5), 0)
+
+		maskRed = cv.morphologyEx(maskRed, cv.MORPH_OPEN, openingKernelRed)
+		maskRed = cv.GaussianBlur(maskRed, (5, 5), 0)
+
+		# Using threshold incase the image is not binary, can be removed
+		_, threshGreen = cv.threshold(maskGreen, 100, 255, cv.THRESH_BINARY)
+		_, threshBlue = cv.threshold(maskBlue, 100, 255, cv.THRESH_BINARY)
+		_, threshYellow = cv.threshold(maskYellow, 100, 255, cv.THRESH_BINARY)
+		_, threshRed = cv.threshold(maskRed, 100, 255, cv.THRESH_BINARY)
+
+		_,contoursGreen, _ = cv.findContours(threshGreen, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+		_,contoursBlue, _ = cv.findContours(threshBlue, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+		_,contoursYellow, _ = cv.findContours(threshYellow, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+		_,contoursRed, _ = cv.findContours(threshRed, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+		# Bitwise-AND mask and original image
+		mask = cv.bitwise_or(maskBlue, maskGreen)
+		mask = cv.bitwise_or(mask, maskYellow)
+		mask=cv.bitwise_or(mask, maskRed)
+		res = cv.bitwise_and(frame, frame, mask=mask)
+
+		numGreen = len(contoursGreen)
+		numRed=len(contoursRed)
+		numBlue=len(contoursBlue)
+		numYellow=len(contoursYellow)
+		if(numBlue>5):
+				print("Bottle: True")
+		else:
+				print("Bottle:False")
+		if(numRed>400):
+				print("Box: True")
+		else:
+				print("Box:False")  
+		if(numGreen>4):
+				print("Ball: True")
+		else:
+				print("Ball:False")
+		
+		if(numYellow>2):
+				print("Disc: True")
+		else:
+				print("Disc:False")
+
+		k = cv.waitKey(5) & 0xFF
+		#clear the stream for the next frame
+		rawCapture.truncate(0)
+		if k == 27:
+				break
+
 
 # ===============
 # Motor Variables
@@ -100,6 +226,9 @@ def connectMotor():
 connectGPS()
 connectSerial()
 connectMotor()
+objThreading = threading.Thread(target=objThread)
+objThreading.start()
+
 # Motor.digitalWrite(7, Motor.HIGH)
 # ======
 # Routes
